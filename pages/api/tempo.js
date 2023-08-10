1c0df242155a59bc8a75eb15868ea7aa75d2fe94
Veridiano <veridianobarroso@gmail.com>
2023-08-10 13:41:09 -0500

    api

---
+++ pages/api/tempo.js
@@ -0,0 +1,12 @@
+function tempo(request, response){
+    const dynamicDate = new Date();
+
+    response.json(
+        {
+            date: dynamicDate.toGMTString()
+        }
+    );
+
+}
+
+export default tempo;
